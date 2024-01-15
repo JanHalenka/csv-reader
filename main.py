cb1e1bc50de181492f6cf0eb0cc2b813cdb4702d
HonzaH <halenka.jan@gmail.com>
2024-01-15 11:42:57 +0100

    basic csv operation

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
 
     user_csv = st.file_uploader('Upload your csv file', type='csv')
 
+    if user_csv is not None:
+        user_question = st.text_input('Ask a question about your CSV')
+    
+
 
 if __name__ == '__main__':
     main()
